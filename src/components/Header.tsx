647cb76eef428a343f092875420c231b8c902f53
eatdu0918 <eatdu0918@gmail.com>
2026-02-19 23:50:58 +0900

    헤더 간격 조정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import { ThemeToggle } from './theme-toggle';
 export default function Header() {
     return (
         <header className="py-8 mb-8 border-b border-zinc-200 dark:border-zinc-800">
-            <div className="container mx-auto px-4 max-w-2xl flex justify-between items-center">
+            <div className="container mx-auto px-4 max-w-5xl flex justify-between items-center">
                 <Link href="/" className="text-2xl font-bold tracking-tighter hover:underline">
                     My Dev Blog
                 </Link>
